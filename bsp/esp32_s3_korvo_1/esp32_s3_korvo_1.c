6847a29eb060b4cfd41a44c1ed42c4690821afd5
Pavel Borcin <palec52@gmail.com>
2023-11-08 21:29:43 +0100

    add spiffs

--- bsp/esp32_s3_korvo_1/esp32_s3_korvo_1.c
+++ bsp/esp32_s3_korvo_1/esp32_s3_korvo_1.c
@@ -11,8 +11,9 @@
 #include "iot_button.h"
 #include "bsp/esp-bsp.h"
 #include "bsp_err_check.h"
+#include "esp_spiffs.h"
 
-static const char *TAG = "ESP32-S3-Korvo-1";
+static const char *TAG = "S3-Korvo-1";
 
 static led_strip_handle_t led_strip;
 
@@ -154,4 +155,37 @@ esp_err_t bsp_led_rgb_set(uint8_t r, uint8_t g, uint8_t b)
 
     ret |= led_strip_refresh(led_strip);
     return ret;
+}
+
+esp_err_t bsp_spiffs_mount(void)
+{
+    esp_vfs_spiffs_conf_t conf = {
+        .base_path = CONFIG_BSP_SPIFFS_MOUNT_POINT,
+        .partition_label = CONFIG_BSP_SPIFFS_PARTITION_LABEL,
+        .max_files = CONFIG_BSP_SPIFFS_MAX_FILES,
+#ifdef CONFIG_BSP_SPIFFS_FORMAT_ON_MOUNT_FAIL
+        .format_if_mount_failed = true,
+#else
+        .format_if_mount_failed = false,
+#endif
+    };
+
+    esp_err_t ret_val = esp_vfs_spiffs_register(&conf);
+
+    BSP_ERROR_CHECK_RETURN_ERR(ret_val);
+
+    size_t total = 0, used = 0;
+    ret_val = esp_spiffs_info(conf.partition_label, &total, &used);
+    if (ret_val != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret_val));
+    } else {
+        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+    }
+
+    return ret_val;
+}
+
+esp_err_t bsp_spiffs_unmount(void)
+{
+    return esp_vfs_spiffs_unregister(CONFIG_BSP_SPIFFS_PARTITION_LABEL);
 }
